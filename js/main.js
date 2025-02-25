e7c2a769b96234837f9df202ca7c05f7484e242d
@GrizzlyKamen <=>
2025-02-25 11:31:11 -0600

    condition created with switch cases and a default case inside a function

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function tengoClases (dia) {
+    switch (dia){
+        case 'lunes':
+            /*console.log('tienes clases con César.');
+            break;
+            esto ya no va para cuando los dos casos cumplen el mismo fin*/
+        case 'martes':
+            console.log('Tienes clases con César.');
+            break;
+        default:
+            console.log('tienes clases, pero no sé con quién.');
+    }
+}
